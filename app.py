f02565a8c968dfb0db376241ab672b65a38fd752
jasleen <jasleenkaur0326@gmail.com>
2022-09-28 00:36:21 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for word in text:
+        if word.isalnum():
+            y.append(word)
+
+    text = y[:]
+    y.clear()
+
+    for word in text:
+        if word not in stopwords.words('english') and word not in string.punctuation:
+            y.append(word)
+
+    text = y[:]
+    y.clear()
+
+    for word in text:
+        y.append(ps.stem(word))
+
+    return " ".join(y)
+
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("SMS SPAM VS HAM CLASSIFIER")
+input_sms = st.text_input("Enter your SMS")
+
+if st.button('SPAM OR HAM'):
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("SPAM")
+    else:
+        st.header("HAM")
